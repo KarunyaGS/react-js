f956b30a36851406b5beb7502c7176535f1286a8
KARUNYA G S <151600121+KarunyaGS@users.noreply.github.com>
2024-05-14 11:53:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import {
+  Component
+}
+from "react";
+class app extends Component{
+  render()
+{
+  return <h1>hi</h1>
+}}
+export default app;
---
+++ contact.js
@@ -0,0 +1,8 @@
+
+import React from 'react';
+
+function contact() {
+  return <h2>Contact</h2>;
+}
+
+export default contact;
